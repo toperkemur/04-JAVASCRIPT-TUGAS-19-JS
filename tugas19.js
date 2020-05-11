3141a9c5caf6e95e8700b31e87376c311e42b81e
Christopher Andrew Kemur <christopher.kemur@gmail.com>
2020-05-12 02:14:59 +0700

    Initial Commit
    Menambahkan tugas19.js

---
+++ tugas19.js
@@ -0,0 +1,15 @@
+var semangat = "Semangat...";
+console.log("TUGAS 1:");
+console.log(semangat.repeat(5));
+console.log("=======================================");
+var bintang = "*";
+console.log("TUGAS 2");
+console.log(bintang.repeat(5));
+console.log(bintang.repeat(20));
+console.log(bintang.repeat(40));
+console.log(bintang.repeat(60));
+console.log(bintang.repeat(100));
+console.log(bintang.repeat(60));
+console.log(bintang.repeat(40));
+console.log(bintang.repeat(20));
+console.log(bintang.repeat(5));
